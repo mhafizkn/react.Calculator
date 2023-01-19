793773e8f7767c889b1c16c7b5cd3011d7a5b78e
mhafizkn <hafizkn98@gmail.com>
2023-01-19 21:49:08 +0800

    Changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './fonts/Kanit-Regular.ttf';
 
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+/* Deprecated
 ReactDOM.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
     document.getElementById("root")
-)
+) */
